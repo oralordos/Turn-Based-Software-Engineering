3b16620415c0975c3ee9b1c62de2f99ccf70f261
Daniel <oralordos@mail.fresnostate.edu>
2014-09-09 09:04:19 -0700

    Inital Project

---
+++ android/src/edu/fresnostate/turnbased/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.fresnostate.turnbased.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.fresnostate.turnbased.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
