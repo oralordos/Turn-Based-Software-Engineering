7613ebf398aa27ab37558b34d3ed3f4fe3c4aa29
Daniel <oralordos@mail.fresnostate.edu>
2014-09-16 15:45:40 -0700

    Fixed the loading and drawing code for the map.

--- core/src/edu/fresnostate/turnbased/Game.java
+++ core/src/edu/fresnostate/turnbased/Game.java
@@ -3,30 +3,36 @@ package edu.fresnostate.turnbased;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	//Texture img;
 	TiledMap map;
 	OrthogonalTiledMapRenderer renderer;
+	OrthographicCamera cam;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		//img = new Texture("badlogic.jpg");
-		map = new TmxMapLoader().load("assets/test.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map, 1/16f);
+		map = new TmxMapLoader().load("test.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
+		cam = new OrthographicCamera(640, 480);
+		cam.translate(new Vector2(320, 240));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		renderer.setView(cam);
 		renderer.render();
 		//batch.begin();
 		//batch.draw(img, 0, 0);
