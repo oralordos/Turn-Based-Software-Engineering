95fa01fcb91da79ac1a5b70851ce575b5f3e7220
Daniel <oralordos@mail.fresnostate.edu>
2014-09-16 15:35:21 -0700

    Added test map loading and drawing.

--- core/src/edu/fresnostate/turnbased/Game.java
+++ core/src/edu/fresnostate/turnbased/Game.java
@@ -5,23 +5,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	//Texture img;
+	TiledMap map;
+	OrthogonalTiledMapRenderer renderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+		map = new TmxMapLoader().load("assets/test.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map, 1/16f);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		renderer.render();
+		//batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
 	}
 }
