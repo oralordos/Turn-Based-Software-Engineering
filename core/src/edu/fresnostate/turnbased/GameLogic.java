d8a77740b3ee4386d6fbdd6fa9e5e1f73e8b8706
shenung <shenung@mail.fresnostate.edu>
2014-10-10 10:20:45 -0700

    gameLogic skeleton

--- core/src/edu/fresnostate/turnbased/GameLogic.java
+++ core/src/edu/fresnostate/turnbased/GameLogic.java
@@ -1,6 +1,46 @@
 package edu.fresnostate.turnbased;
 
-public class GameLogic
+public class GameLogic implements EventListener
 {
-	
+	public GameLogic ()
+	{
+		EventManager manager = EventManager.get ();
+		manager.addListener (this, EventType.DestroyUnit);
+		manager.addListener (this, EventType.CreateUnit);
+		manager.addListener (this, EventType.ChangeCurrentPlayer);
+	}
+
+	@Override
+	public void receiveEvent (Event e)
+	{
+		switch (e.getEventType ())
+		{
+		case DestroyUnit :
+			handleDestroyUnit ((DestroyUnitEvent) e);
+			break;
+		case CreateUnit :
+			handleCreateUnit ((CreateUnitEvent) e);
+			break;
+		case ChangeCurrentPlayer :
+			handleChangeCurrentPlayer((ChangeCurrentPlayerEvent)e);
+			break;
+		default :
+			break;
+		}
+	}
+
+	private void handleChangeCurrentPlayer (ChangeCurrentPlayerEvent e)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	private void handleCreateUnit (CreateUnitEvent e)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	private void handleDestroyUnit (DestroyUnitEvent due)
+	{
+		// TODO Auto-generated method stub
+	}
 }
