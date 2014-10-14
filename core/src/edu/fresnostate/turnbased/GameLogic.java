63fc4abff57c407831d986381912269b4eb321ef
shenung <shenung@mail.fresnostate.edu>
2014-10-14 13:54:00 -0700

    Removed unused events

--- core/src/edu/fresnostate/turnbased/GameLogic.java
+++ core/src/edu/fresnostate/turnbased/GameLogic.java
@@ -5,9 +5,7 @@ public class GameLogic implements EventListener
 	public GameLogic ()
 	{
 		EventManager manager = EventManager.get ();
-		manager.addListener (this, EventType.UNIT_DESTROYED);
 		manager.addListener (this, EventType.CREATE_UNIT);
-		manager.addListener (this, EventType.CURRENT_PLAYER_CHANGED);
 	}
 
 	@Override
@@ -15,31 +13,15 @@ public class GameLogic implements EventListener
 	{
 		switch (e.getEventType ())
 		{
-		case UNIT_DESTROYED :
-			handleDestroyUnit ((UnitDestroyedEvent) e);
-			break;
 		case CREATE_UNIT :
 			handleCreateUnit ((CreateUnitEvent) e);
-			break;
-		case CURRENT_PLAYER_CHANGED :
-			handleChangeCurrentPlayer((CurrentPlayerChangedEvent)e);
 			break;
 		default :
 			break;
 		}
 	}
 
-	private void handleChangeCurrentPlayer (CurrentPlayerChangedEvent e)
-	{
-		// TODO Auto-generated method stub
-	}
-
 	private void handleCreateUnit (CreateUnitEvent e)
-	{
-		// TODO Auto-generated method stub
-	}
-
-	private void handleDestroyUnit (UnitDestroyedEvent due)
 	{
 		// TODO Auto-generated method stub
 	}
