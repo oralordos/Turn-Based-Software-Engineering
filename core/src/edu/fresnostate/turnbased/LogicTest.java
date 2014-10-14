ba50a833b566fac49ff5df878250ebfc0a2be660
shenung <shenung@mail.fresnostate.edu>
2014-10-14 15:43:08 -0700

    J Unit for gameLogic

---
+++ core/src/edu/fresnostate/turnbased/LogicTest.java
@@ -0,0 +1,27 @@
+package edu.fresnostate.turnbased;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class LogicTest extends TestCase
+{
+	@Before
+	protected void setUp () throws Exception
+	{
+		super.setUp ();
+	}
+
+	@After
+	protected void tearDown () throws Exception
+	{
+		super.tearDown ();
+	}
+
+	@Test
+	public void test ()
+	{
+		fail ("Not yet implemented");
+	}
+}
