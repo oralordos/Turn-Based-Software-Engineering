be36e97924fee7a5e41fb0ebc6c9e0180baf591d
Daniel <oralordos@mail.fresnostate.edu>
2014-10-14 15:46:07 -0700

    Added first event manager test case.

--- core/src/edu/fresnostate/turnbased/EventManagerTest.java
+++ core/src/edu/fresnostate/turnbased/EventManagerTest.java
@@ -5,23 +5,44 @@ import org.junit.Before;
 import org.junit.Test;
 import junit.framework.TestCase;
 
-public class EventManagerTest extends TestCase
+
+public class EventManagerTest extends TestCase implements EventListener
 {
+	private EventManager	manager;
+	private int				unitDestroyedID;
+
 	@Before
 	protected void setUp () throws Exception
 	{
 		super.setUp ();
+		EventManager.create ();
+		manager = EventManager.get ();
+		manager.addListener (this, EventType.UNIT_DESTROYED);
+		unitDestroyedID = - 1;
 	}
 
 	@After
 	protected void tearDown () throws Exception
 	{
 		super.tearDown ();
+		manager.dispose ();
 	}
 
 	@Test
 	public void testQueueEvent ()
 	{
-		fail ("Not yet implemented");
+		UnitDestroyedEvent e = new UnitDestroyedEvent (0);
+		manager.queueEvent (e);
+		manager.processEvents ();
+		assertEquals (unitDestroyedID, 0);
+	}
+
+	@Override
+	public void receiveEvent (Event e)
+	{
+		if (e.getEventType () == EventType.UNIT_DESTROYED)
+		{
+			unitDestroyedID = ((UnitDestroyedEvent) e).unitID;
+		}
 	}
 }
