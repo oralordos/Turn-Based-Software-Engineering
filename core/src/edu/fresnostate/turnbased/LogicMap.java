49ebd6cde4bdc9ef18461bee46b89155377e5826
Guillermo Bueno <bmemo2@mail.fresnostate.edu>
2014-10-16 15:43:16 -0700

    Created Tile Map

---
+++ core/src/edu/fresnostate/turnbased/LogicMap.java
@@ -0,0 +1,28 @@
+package edu.fresnostate.turnbased;
+
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+
+
+public class LogicMap {
+	
+	
+	public  LogicMap(TiledMap t)
+	{
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
