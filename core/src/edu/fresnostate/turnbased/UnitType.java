229a76f939a05f6fc7448b98801ec3c3d44e1349
Daniel <oralordos@mail.fresnostate.edu>
2014-10-22 22:01:14 -0700

    Renamed UnitTyp to UnitType.

---
+++ core/src/edu/fresnostate/turnbased/UnitType.java
@@ -0,0 +1,40 @@
+package edu.fresnostate.turnbased;
+
+public class UnitType 
+{
+	int UnitID;
+	int UnitBaseHP;
+	int UnitCurentHP;
+	int UnitAD;
+	int UnitMovement;
+	int UnitDF;
+	{
+	 
+		 UnitID=1; 
+		 switch (UnitID)
+		 {
+		 case 1: 
+			 UnitBaseHP=10 ;
+			 UnitCurentHP=10;
+			 UnitAD=1;
+			 UnitMovement=3;
+			 UnitDF=1;
+			 break;
+		 case 2:
+			 UnitBaseHP=10 ;
+			 UnitCurentHP=10;
+			 UnitAD=1;
+			 UnitMovement=4;
+			 UnitDF=2;
+			 break;
+		 case 3:
+			 UnitBaseHP=10 ;
+			 UnitCurentHP=10;
+			 UnitAD=1;
+			 UnitMovement=2;
+			 UnitDF=3;
+			 break;
+		 }
+	 }
+}
+
