e028011c126e1cacf7cd9b45762e42d5b1e2b97f
Shenung <shenung@mail.fresnostate.edu>
2014-10-24 00:50:42 -0700

    Created Player Class but needs some revision possibly

---
+++ core/src/edu/fresnostate/turnbased/Player.java
@@ -0,0 +1,14 @@
+package edu.fresnostate.turnbased;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Player 
+{
+	public void PlayerList(LinkedList<Player> PlayerList)
+	{
+		PlayerList = new LinkedList<Player>;
+		Map<ResourceType, Integer> resources = new HashMap<ResourceType, Integer>;
+	}
+}
