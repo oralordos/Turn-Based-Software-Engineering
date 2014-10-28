181be54b41e223245417a7420b6899d28c37940b
Daniel <oralordos@mail.fresnostate.edu>
2014-10-28 15:13:50 -0700

    Fixed problems in PathfindingMap relating to Coordinates.

--- core/src/edu/fresnostate/turnbased/pathfinding/PathfindingMap.java
+++ core/src/edu/fresnostate/turnbased/pathfinding/PathfindingMap.java
@@ -7,15 +7,15 @@ import edu.fresnostate.turnbased.Coordinates;
 
 public class PathfindingMap
 {
-	private List <List <List <Coordinates>>>	paths;
+	private List <List <List <Coordinates <Integer>>>>	paths;
 
 	public PathfindingMap (List <List <Integer>> pathData, TilemapGraph graph,
 			int width, int height)
 	{
-		paths = new ArrayList <List <List <Coordinates>>> ();
+		paths = new ArrayList <List <List <Coordinates <Integer>>>> ();
 		for (int x = 0; x < width; ++ x)
 		{
-			paths.add (new ArrayList <List <Coordinates>> ());
+			paths.add (new ArrayList <List <Coordinates <Integer>>> ());
 			for (int y = 0; y < height; ++ y)
 			{
 				paths.get (x).add (null);
@@ -29,13 +29,14 @@ public class PathfindingMap
 				int pathTo = path.get (path.size () - 1);
 				int pathToX = graph.getLocalizedX (pathTo);
 				int pathToY = graph.getLocalizedY (pathTo);
-				List <Coordinates> localizedPath = localizePath (graph, path);
+				List <Coordinates <Integer>> localizedPath =
+						localizePath (graph, path);
 				paths.get (pathToX).set (pathToY, localizedPath);
 			}
 		}
 	}
 
-	public List <Coordinates> getPathTo (int x, int y)
+	public List <Coordinates <Integer>> getPathTo (int x, int y)
 	{
 		return paths.get (x).get (y);
 	}
@@ -45,14 +46,15 @@ public class PathfindingMap
 		return paths.get (x).get (y) != null;
 	}
 
-	private static List <Coordinates> localizePath (TilemapGraph graph,
-			List <Integer> pathData)
+	private static List <Coordinates <Integer>> localizePath (
+			TilemapGraph graph, List <Integer> pathData)
 	{
-		List <Coordinates> path = new ArrayList <Coordinates> ();
+		List <Coordinates <Integer>> path =
+				new ArrayList <Coordinates <Integer>> ();
 		for (Integer node : pathData)
 		{
-			path.add (new Coordinates (graph.getLocalizedX (node), graph
-					.getLocalizedY (node)));
+			path.add (new Coordinates <Integer> (graph.getLocalizedX (node),
+					graph.getLocalizedY (node)));
 		}
 		return path;
 	}
