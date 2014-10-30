5c601eedc2a3dd4bfeed40c78340c8676411abdc
Daniel <oralordos@mail.fresnostate.edu>
2014-10-30 13:00:56 -0700

    Added additional functions to information provider interface.

--- core/src/edu/fresnostate/turnbased/event/InformationProvider.java
+++ core/src/edu/fresnostate/turnbased/event/InformationProvider.java
@@ -1,6 +1,7 @@
 package edu.fresnostate.turnbased.event;
 
 import edu.fresnostate.turnbased.Player;
+import edu.fresnostate.turnbased.Tile;
 import edu.fresnostate.turnbased.Unit;
 import edu.fresnostate.turnbased.pathfinding.PathfindingMap;
 
@@ -9,4 +10,8 @@ public interface InformationProvider
 	public PathfindingMap getPathMap(int unitID);
 	public Unit getUnit(int unitID);
 	public Player getPlayer(int playerNum);
+	public int getNumberOfPlayers();
+	public int getMapWidth();
+	public int getMapHeight();
+	public Tile getTile(int x, int y);
 }
