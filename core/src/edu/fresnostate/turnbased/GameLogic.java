c8170e7c3faa7ac55d43a08b43dadff4082d1a2d
shenung <shenung@mail.fresnostate.edu>
2014-11-04 15:44:35 -0800

    Changed player to int inside CurrentPlayerChange event
    Finished CreateUnitEvent and EndTurnEvent inside game Logic

--- core/src/edu/fresnostate/turnbased/GameLogic.java
+++ core/src/edu/fresnostate/turnbased/GameLogic.java
@@ -1,24 +1,30 @@
 package edu.fresnostate.turnbased;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import edu.fresnostate.turnbased.event.AttackUnitEvent;
 import edu.fresnostate.turnbased.event.CreateUnitEvent;
+import edu.fresnostate.turnbased.event.CurrentPlayerChangedEvent;
 import edu.fresnostate.turnbased.event.EndTurnEvent;
 import edu.fresnostate.turnbased.event.Event;
 import edu.fresnostate.turnbased.event.EventListener;
 import edu.fresnostate.turnbased.event.EventManager;
 import edu.fresnostate.turnbased.event.EventType;
+import edu.fresnostate.turnbased.event.InformationProvider;
 import edu.fresnostate.turnbased.event.UnitAttackedEvent;
 import edu.fresnostate.turnbased.event.UnitCreatedEvent;
+import edu.fresnostate.turnbased.pathfinding.PathfindingMap;
 
 
-public class GameLogic implements EventListener
+public class GameLogic implements EventListener, InformationProvider
 {
 	private List <Player>		playerList;
 	private Map <Integer, Unit>	units;
+	private LogicMap			map;
+	private int					Currentplayer;
 
 	public GameLogic ()
 	{
@@ -48,8 +54,8 @@ public class GameLogic implements EventListener
 
 	private void handleEndTurn (EndTurnEvent e)
 	{
-		// TODO Finish EndTurn
-		//EventManager.queueEvent (new EndTurnEvent (e));
+		Currentplayer = (Currentplayer + 1) % getNumberPlayers ();
+		EventManager.queueEvent (new CurrentPlayerChangedEvent (Currentplayer));
 	}
 
 	private void handleAttackUnit (AttackUnitEvent e)
@@ -64,34 +70,51 @@ public class GameLogic implements EventListener
 		}
 	}
 
+	/**
+	 * This handles CreateUnitEvent events
+	 * 
+	 * @param e
+	 *            The event to handle
+	 */
 	private void handleCreateUnit (CreateUnitEvent e)
 	{
-		// TODO Make sure player is current player and make sure player has enough cost.
-		Player.resources.get(ResourceType) 
-		if()
+		// TODO Make sure player is current player and make sure player has
+		// enough cost and play is owns tile.
+		Player player = getPlayer (Currentplayer);
+		int playerMoney = player.resources.get (ResourceType.MONEY);
+		int playerFood = player.resources.get (ResourceType.FOOD);
+		int moneyCost = e.unitType.costs.get (ResourceType.MONEY);
+		int foodCost = e.unitType.costs.get (ResourceType.FOOD);
+		if (moneyCost <= playerMoney && foodCost <= playerFood)
 		{
-			Player PlayerResorce = 
-		
-			EventManager.queueEvent (new UnitCreatedEvent (unit.unitID));
+			player.resources.put (ResourceType.MONEY, playerMoney - moneyCost);
+			player.resources.put (ResourceType.FOOD, playerFood - foodCost);
+			Unit newUnit = e.unitType.create (Currentplayer);
+			units.put (newUnit.unitID, newUnit);
+			EventManager.queueEvent (new UnitCreatedEvent (newUnit.unitID));
 		}
 	}
 
-	private Unit getUnit (int unitID)
+	@Override
+	public Unit getUnit (int unitID)
 	{
 		return units.get (unitID);
 	}
-	
-	private Player getPathMap()
+
+	@Override
+	public PathfindingMap getPathMap (int unitID)
 	{
-		// TODO 
+		Unit unit = getUnit (unitID);
+		return map.getPathfindingMap (unit.x, unit.y, unit.type.movement,
+				unit.type.move);
 	}
-	
-	private Player getPlayer(List <Player> )
+
+	@Override
+	public Player getPlayer (int playerNum)
 	{
-		return Player;
+		return playerList.get (playerNum);
 	}
-	
-	
+
 	public List <Player> getPlayerList ()
 	{
 		return playerList;
@@ -100,5 +123,29 @@ public class GameLogic implements EventListener
 	private void setPlayerList (List <Player> playerList)
 	{
 		this.playerList = playerList;
+	}
+
+	@Override
+	public int getNumberPlayers ()
+	{
+		return playerList.size ();
+	}
+
+	@Override
+	public int getMapWidth ()
+	{
+		return map.getWidth ();
+	}
+
+	@Override
+	public int getMapHeight ()
+	{
+		return map.getHeight ();
+	}
+
+	@Override
+	public Tile getMapTile (int x, int y)
+	{
+		return map.getTile (x, y);
 	}
 }
