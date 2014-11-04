535705854633e0a8e312926e42d65cc8004dd925
Marco <tonoav@gmail.com>
2014-11-04 15:16:42 -0800

    Camera and PlayerView

--- core/src/edu/fresnostate/turnbased/Camera.java
+++ core/src/edu/fresnostate/turnbased/Camera.java
@@ -1,26 +1,62 @@
 package edu.fresnostate.turnbased;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 
 
 public class Camera
 {
-	private OrthographicCamera cam;
-	public void zoom(float x, float y) {
-		zoom (x, y);		
+	private OrthographicCamera	cam;
+
+	public Camera (float w, float h)
+	{
+		cam = new OrthographicCamera (w, h);
 	}
-	public void move (float screenX, float screenY){
-		cam.getPickRay (screenX, screenY);
-		
+
+	public void zoom (float z)
+	{
+		cam.zoom *= z;
 	}
-	public void moveTo (float x, float y){
+
+	public void resize (float width, float height)
+	{
+		cam.viewportWidth = width;
+		cam.viewportHeight = height;
 	}
-	public void toGameCoords (float screenX, float screenY) {
-		cam.getPickRay (screenX, screenY);
-		
+
+	public void move (float screenX, float screenY)
+	{
+		cam.translate (screenX * cam.zoom, screenY * cam.zoom);
 	}
-	public void fromGameCoords () {
-		
+
+	public void moveTo (float x, float y)
+	{
+	}
+
+	public void handleZoom ()
+	{ // Keeps the camera within the bounds of the map.
+		float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+		float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+		cam.zoom = MathUtils.clamp (cam.zoom, 0.1f, 100 / cam.viewportWidth);
+		cam.position.x =
+				MathUtils.clamp (cam.position.x, effectiveViewportWidth / 2f,
+						100 - effectiveViewportWidth / 2f);
+		cam.position.y =
+				MathUtils.clamp (cam.position.y, effectiveViewportHeight / 2f,
+						100 - effectiveViewportHeight / 2f);
+	}
+
+	public Coordinates <Float> toGameCoords (float screenX, float screenY)
+	{
+		Vector3 vector = new Vector3 ();
+		cam.getPickRay (screenX, screenY).getEndPoint (vector, 0);
+		return new Coordinates <Float> (vector.x, vector.y);
+	}
+
+	public Coordinates <Float> fromGameCoords (float x, float y)
+	{
+		Vector3 vector = cam.project (new Vector3 (x, y, 0));
+		return new Coordinates <Float> (vector.x, vector.y);
 	}
 }
--- core/src/edu/fresnostate/turnbased/PlayerView.java
+++ core/src/edu/fresnostate/turnbased/PlayerView.java
@@ -1,63 +1,189 @@
 package edu.fresnostate.turnbased;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.assets.loaders.resolvers.ExternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.SerializationException;
 
-public class PlayerView implements ApplicationListener {
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	public void draw () {
-		
-	}
-	public void handleInput () {
-		
-	}
+
+public class PlayerView implements View, Disposable, GestureListener
+{
+	private final static float			MOUSE_WHEEL_ZOOM	= 1.25f;
+	private TiledMap					map;
+	private OrthogonalTiledMapRenderer	renderer;
+	private int							tileSize;
+	private InputMultiplexer			multiplexer;
+	private float						lastInitial			= - 1.0f;
+	private float						lastZoom			= - 1.0f;
+	private Camera						cam;
+	private InputAdapter				adapter				=
+																	new InputAdapter ()
+																	{
+																		@Override
+																		public
+																				boolean
+																				scrolled (
+																						int amount)
+																		{
+																			if (amount > 0)
+																			{
+																				cam.zoom (MOUSE_WHEEL_ZOOM);
+																			}
+																			else
+																			{
+																				cam.zoom (1 / MOUSE_WHEEL_ZOOM);
+																			}
+																			return true;
+																		}
+																	};
+
 	@Override
-	public void create ()
+	public boolean zoom (float initialDistance, float distance)
 	{
-		// TODO Auto-generated method stub
-		batch = new SpriteBatch();
-		texture = new Texture(Gdx.files.internal("data/file.png"));
-		sprite = new Sprite(texture);
-		
+		if (lastInitial != initialDistance)
+		{
+			lastInitial = initialDistance;
+			lastZoom = 1.0f;
+		}
+		float zoom = initialDistance / distance;
+		cam.zoom (zoom / lastZoom);
+		lastZoom = zoom;
+		return true;
 	}
+
 	@Override
+	public boolean touchDown (float x, float y, int pointer, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean tap (float x, float y, int count, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean pinch (Vector2 initialPointer1, Vector2 initialPointer2,
+			Vector2 pointer1, Vector2 pointer2)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean panStop (float x, float y, int pointer, int button)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean pan (float x, float y, float deltaX, float deltaY)
+	{
+		cam.move ( - deltaX / tileSize, deltaY / tileSize);
+		return true;
+	}
+
+	@Override
+	public boolean longPress (float x, float y)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean fling (float velocityX, float velocityY, int button)
+	{
+		return false;
+	}
+
+	public PlayerView ()
+	{
+		try
+		{
+			map =
+					new TmxMapLoader (new ExternalFileHandleResolver ())
+							.load ("test.tmx");
+		}
+		catch (SerializationException e)
+		{
+			map = new TmxMapLoader ().load ("test.tmx");
+		}
+		tileSize =
+				map.getTileSets ().getTile (1).getTextureRegion ()
+						.getRegionWidth ();
+		renderer = new OrthogonalTiledMapRenderer (map, 1.0f / tileSize);
+		float w = Gdx.graphics.getWidth ();
+		float h = Gdx.graphics.getHeight ();
+		cam = new Camera (w, h);
+		cam.zoom (2.0f);
+		multiplexer = new InputMultiplexer ();
+		multiplexer.addProcessor (new GestureDetector (this));
+		multiplexer.addProcessor (adapter);
+		Gdx.input.setInputProcessor (multiplexer);
+		Gdx.gl.glClearColor (0, 0, 0, 1);
+	}
+
+	public void handleInput ()
+	{
+		cam.handleZoom (); // Controls the camera by updating its zoom and
+							// position.
+		if (Gdx.input.isKeyPressed (Input.Keys.ESCAPE))
+			Gdx.app.exit ();
+		if (Gdx.input.isKeyPressed (Input.Keys.LEFT))
+			cam.move ( - 2, 0);
+		if (Gdx.input.isKeyPressed (Input.Keys.RIGHT))
+			cam.move (2, 0);
+		if (Gdx.input.isKeyPressed (Input.Keys.UP))
+			cam.move (0, 2);
+		if (Gdx.input.isKeyPressed (Input.Keys.DOWN))
+			cam.move (0, - 2);
+		if (Gdx.input.isKeyPressed (Input.Keys.NUM_1))
+			map.getLayers ().get (0)
+					.setVisible ( ! map.getLayers ().get (0).isVisible ());
+		if (Gdx.input.isKeyPressed (Input.Keys.NUM_2))
+			map.getLayers ().get (1)
+					.setVisible ( ! map.getLayers ().get (1).isVisible ());
+	}
+
 	public void resize (int width, int height)
 	{
-		// TODO Auto-generated method stub
-		
+		cam.resize (30f, 30f * height / width);
 	}
-	@Override
-	public void render (){
-		// TODO Auto-generated method stub
-		Gdx.gl.glClearColor (1, 1, 1, 1);
-		Gdx.gl.glClear ((GL20.GL_COLOR_BUFFER_BIT));
-		
-		batch.begin();
-		sprite.draw (batch);
-		batch.end ();
-	}
-	@Override
-	public void pause ()
+
+	public void render (TiledMap map, int tileSize,
+			OrthogonalTiledMapRenderer renderer, OrthographicCamera cam,
+			InputMultiplexer multiplexer, InputProcessor adapter,
+			GestureListener listener)
 	{
-		// TODO Auto-generated method stub
-		
+		// TODO draw units
+		// TODO draw GUI
+		Gdx.gl.glClear (GL20.GL_COLOR_BUFFER_BIT);
+		cam.update ();
+		renderer.setView (cam);
+		renderer.render ();
 	}
-	@Override
-	public void resume ()
-	{
-		// TODO Auto-generated method stub
-		
-	}
+
 	@Override
 	public void dispose ()
 	{
-		// TODO Auto-generated method stub
-		
+		renderer.dispose ();
+		map.dispose ();
+	}
+
+	@Override
+	public void update ()
+	{
+		// TODO Loop through graphical units and call their update functions.
 	}
 }
