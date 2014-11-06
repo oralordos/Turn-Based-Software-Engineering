0e1d5b149367fb5ae94b896bd512f34eb663ecd4
Patrick Whelan <Patrick Whelan@PatrickWhelan.swlan.csufresno.edu>
2014-11-06 18:18:04 -0500

    UPdate

--- core/src/edu/fresnostate/turnbased/UnitType.java
+++ core/src/edu/fresnostate/turnbased/UnitType.java
@@ -3,19 +3,23 @@ package edu.fresnostate.turnbased;
 public class UnitType {
 
 	public enum Unitstates {
-		INFANTRY(10, 2, 3, 4), TANK(10, 4, 4, 4), MONSTER(10, 3, 4, 5);
+		INFANTRY(10, 2, 3, 4,1,200), TANK(10, 4, 4, 4,1,1000), MONSTER(10, 3, 4, 5,2,1200);
 
 		final int UnitBaseHP;
 
 		final int UnitAD;
 		final int UnitMovement;
 		final int UnitDF;
+		final int UnitRang ;
+		final int Unitcost;
 
-		Unitstates(int UnitBaseHP, int UnitDF, int UnitAD, int UnitMovement) {
+		Unitstates(int UnitBaseHP, int UnitDF, int UnitAD, int UnitMovement,int UnitRang,int Unitcost) {
 			this.UnitBaseHP = UnitBaseHP;
 			this.UnitDF = UnitDF;
 			this.UnitAD = UnitAD;
 			this.UnitMovement = UnitMovement;
+			this.UnitRang=UnitRang;
+			this.Unitcost=Unitcost;
 		}
 
 	}
