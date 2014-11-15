a25a617680b372e6bc873fd385473db9bd45e38d
Daniel <oralordos@mail.fresnostate.edu>
2014-11-14 16:31:22 -0800

    Changed Game class to a finite state machine.

---
+++ core/src/edu/fresnostate/turnbased/MainMenuState.java
@@ -0,0 +1,34 @@
+package edu.fresnostate.turnbased;
+
+public class MainMenuState implements GameState
+{
+	@Override
+	public void enter ()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void exit ()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void resize (int width, int height)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void update ()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void render ()
+	{
+		// TODO Auto-generated method stub
+	}
+}
