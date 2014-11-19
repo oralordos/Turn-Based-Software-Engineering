8c562c40ec73dd5a9951d5dcf38fb29af071bd36
Patrick Whelan <pawhelan@att.net>
2014-11-19 14:31:14 -0500

    Sup

--- core/src/edu/fresnostate/turnbased/Unit.java
+++ core/src/edu/fresnostate/turnbased/Unit.java
@@ -3,13 +3,15 @@ package edu.fresnostate.turnbased;
 
 import java.util.List;
 
+import edu.fresnostate.turnbased.event.EventManager;
+
 public class Unit 
 {
-
+	public final UnitType type;
 	int UnitcurentHP;
 	int UnitId;
 	int BilldingCurentHP;
-	
+	int x,y;
 
 	public Unit (UnitType unitType, int player, int id)
 	{
@@ -21,6 +23,8 @@ public class Unit
 	public void Move(List<Coordinates<Integer>> path)
 	{
 		Coordinates<Integer> lastSpace = path.get(path.size()-1);
+		x=lastSpace.x;
+		y=lastSpace.y;
 	}
 	
 	public void AttackUint()
@@ -56,9 +60,15 @@ public class Unit
 		UnitcurentHP+2;
 	}
 
-	public void CanpathOn()
+	public boolean CanpathOn(List <Coordinates<Integer>>path)
 	{
-		
+		int movedSoFar=0;
+		for(Coordinates<Integer>currentPoint :path)
+		{
+			Tile tile = EventManager. getMapTile(currentPoint.x, currentPoint.y);
+			movedSoFar +=tile.getmovementcost(type.movement);
+		}
+		return movedSoFar <=type.UnitMovement;
 	}
 	
 	
