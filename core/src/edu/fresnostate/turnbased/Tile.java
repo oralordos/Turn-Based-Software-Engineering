333f171509f8c934df20d5bf823543d2f67e6b58
Daniel <oralordos@mail.fresnostate.edu>
2014-11-25 16:28:32 -0800

    Added images and fixed logicmap loading.

--- core/src/edu/fresnostate/turnbased/Tile.java
+++ core/src/edu/fresnostate/turnbased/Tile.java
@@ -21,24 +21,24 @@ public class Tile
 
 	public Tile (MapProperties p)
 	{
-		Max_health = p.get ("health", Integer.class);
+		Max_health = Integer.parseInt (p.get ("health", String.class));
 		Cur_health = Max_health;
-		Defence = p.get ("defence", Integer.class);
-		canCapture = p.get ("canCapture", Boolean.class);
-		capturePoints = p.get ("capturePoints", Integer.class);
-		unitRegen = p.get ("unitRegen", Integer.class);
-		resourceRadius = p.get ("resourceRadius", Integer.class);
+		Defence = Integer.parseInt (p.get ("defence", String.class));
+		canCapture = Boolean.parseBoolean (p.get ("canCapture", String.class));
+		capturePoints = Integer.parseInt (p.get ("capturePoints", String.class));
+		unitRegen = Integer.parseInt(p.get ("unitRegen", String.class));
+		resourceRadius = Integer.parseInt(p.get ("resourceRadius", String.class));
 		unitOnID = - 1;
 
 		income = new EnumMap <ResourceType, Integer> (ResourceType.class);
-		income.put (ResourceType.FOOD, p.get ("incomeFood", Integer.class));
-		income.put (ResourceType.MONEY, p.get ("incomeMoney", Integer.class));
+		income.put (ResourceType.FOOD, Integer.parseInt (p.get ("incomeFood", String.class)));
+		income.put (ResourceType.MONEY, Integer.parseInt (p.get ("incomeMoney", String.class)));
 
 		Costs = new EnumMap <MovementType, Integer> (MovementType.class);
-		Costs.put (MovementType.WALK, p.get ("movementWalk", Integer.class));
-		Costs.put (MovementType.FLY, p.get ("movementFly", Integer.class));
-		Costs.put (MovementType.TREAD, p.get ("movementTread", Integer.class));
-		Costs.put (MovementType.SAIL, p.get ("movementSail", Integer.class));
+		Costs.put (MovementType.WALK, Integer.parseInt (p.get ("movementWalk", String.class)));
+		Costs.put (MovementType.FLY, Integer.parseInt (p.get ("movementFly", String.class)));
+		Costs.put (MovementType.TREAD, Integer.parseInt (p.get ("movementTread", String.class)));
+		Costs.put (MovementType.SAIL, Integer.parseInt (p.get ("movementSail", String.class)));
 	}
 
 	public int getMaxHealth ()
